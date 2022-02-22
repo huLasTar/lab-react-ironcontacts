049f04d7c4651d43444c5c4564d1f46ffb2d6119
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-22 17:00:30 +0100

    Iteration 3 done

--- src/App.js
+++ src/App.js
@@ -6,9 +6,34 @@ import { useState } from "react";
 function App() {
   const [getContacts, setContacts] = useState(contacts.slice(0, 5));
 
+  // Iteration 3:
+  function addRandomContact() {
+    const remainingContacts = contacts.filter(
+      (remaining) => !getContacts.includes(remaining)
+    );
+    const random = Math.floor(Math.random() * remainingContacts.length);
+    setContacts(remainingContacts.splice(random, 1).concat(getContacts));
+  }
+
+  // Iteration 4:
+
+  // Iteration 5:
+  const deleteContact = (contactId) => {
+    const filteredContacts = contacts.filter((contact) => {
+      return contact._id !== contactId;
+    });
+
+    setContacts(filteredContacts);
+  };
+
   return (
     <div className="App">
       <h1>IronContacts</h1>
+      <button className="button" onClick={() => addRandomContact()}>
+        Add Random Contact
+      </button>
+      <button className="button">Sort by Popularity</button>
+      <button className="button">Sort by Name</button>
       <table className="contactsTable">
         <thead>
           <tr>
@@ -17,12 +42,13 @@ function App() {
             <th>Popularity</th>
             <th>Won Oscar</th>
             <th>Won Emmy</th>
+            <th>Actions</th>
           </tr>
         </thead>
         <tbody>
-          {getContacts.map((contact) => {
+          {getContacts.map((contact, key) => {
             return (
-              <tr>
+              <tr key={key}>
                 <td className="contactPicture">
                   <img
                     className="contactImg"
@@ -31,12 +57,22 @@ function App() {
                   />
                 </td>
                 <td className="contactName">{contact.name}</td>
-                <td className="contactPopularity">{contact.popularity}</td>
+                <td className="contactPopularity">
+                  {contact.popularity.toFixed(2)}
+                </td>
                 <td className="contactOscar">
-                  {contact.wonOscar ? <p>🏆</p> : <p> </p>}
+                  {contact.wonOscar ? <p>🏆</p> : null}
                 </td>
                 <td className="contactEmmy">
-                  {contact.wonEmmy ? <p>🏆</p> : <p> </p>}
+                  {contact.wonEmmy ? <p>🏆</p> : null}
+                </td>
+                <td className="contactDelete">
+                  <button
+                    onClick={() => deleteContact(contact.id)}
+                    className="deleteButton"
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             );
