541642c320ca5f81ad86ecef81bf1272c1bd5315
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-22 14:44:58 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [getContacts, setContacts] = useState(contacts.slice(0, 5));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IronContacts</h1>
+      <table className="contactsTable">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {getContacts.map((contact) => {
+            return (
+              <tr>
+                <td className="contactPicture">
+                  <img
+                    className="contactImg"
+                    src={contact.pictureUrl}
+                    alt={contact.name}
+                  />
+                </td>
+                <td className="contactName">{contact.name}</td>
+                <td className="contactPopularity">{contact.popularity}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
-
 export default App;
