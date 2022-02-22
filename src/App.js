ecbce131c5cdc9f553d2cbeebc7c30b1b23d9bb7
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-22 17:35:53 +0100

    Iteration 4 done

--- src/App.js
+++ src/App.js
@@ -4,18 +4,36 @@ import contacts from "./contacts.json";
 import { useState } from "react";
 
 function App() {
+  // Iteration 2:
   const [getContacts, setContacts] = useState(contacts.slice(0, 5));
 
   // Iteration 3:
-  function addRandomContact() {
+  const addRandomContact = () => {
     const remainingContacts = contacts.filter(
       (remaining) => !getContacts.includes(remaining)
     );
     const random = Math.floor(Math.random() * remainingContacts.length);
     setContacts(remainingContacts.splice(random, 1).concat(getContacts));
-  }
+  };
 
   // Iteration 4:
+  const sortByName = () => {
+    const allContacts = [...getContacts];
+    // sorted numbers, only with - descending order or + ascending order
+    const sortedByName = allContacts.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContacts(sortedByName);
+  };
+
+  const sortByPopularity = () => {
+    const allContacts = [...getContacts];
+    // sorted numbers, only with - descending order or + ascending order
+    const sortedByPopularity = allContacts.sort(
+      (a, b) => b.popularity - a.popularity
+    );
+    setContacts(sortedByPopularity);
+  };
 
   // Iteration 5:
   const deleteContact = (contactId) => {
@@ -29,11 +47,15 @@ function App() {
   return (
     <div className="App">
       <h1>IronContacts</h1>
-      <button className="button" onClick={() => addRandomContact()}>
+      <button className="button" onClick={addRandomContact}>
         Add Random Contact
       </button>
-      <button className="button">Sort by Popularity</button>
-      <button className="button">Sort by Name</button>
+      <button className="button" onClick={sortByName}>
+        Sort by Name
+      </button>
+      <button className="button" onClick={sortByPopularity}>
+        Sort by Popularity
+      </button>
       <table className="contactsTable">
         <thead>
           <tr>
